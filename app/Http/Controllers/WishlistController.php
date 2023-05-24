cdfa8fead296ca418dd7b71ce00125439988fb70
ibrahemshabanah2022 <113831449+ibrahemshabanah2022@users.noreply.github.com>
2023-05-24 22:07:45 +0200

    remove  .env from git ignore

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -17,13 +17,36 @@ class WishlistController extends Controller
 
     public function store(Request $request)
     {
+        $productId = $request->input('product_id');
+        $userId = $request->user()->id;
+
+        // Check if product already exists in user's wishlist
+        $wishlistItem = Wishlist::where('user_id', $userId)
+            ->where('product_id', $productId)
+            ->first();
+
+        if ($wishlistItem) {
+            // Product already exists in wishlist
+            return response()->json(['message' => 'Product already in wishlist']);
+        }
+
+        // Product doesn't exist in wishlist, create new wishlist item
         $wishlist = new Wishlist;
-        $wishlist->user_id = $request->user()->id;
-        $wishlist->product_id = $request->input('product_id');
+        $wishlist->user_id = $userId;
+        $wishlist->product_id = $productId;
         $wishlist->save();
 
         return response()->json($wishlist);
     }
+    // public function store(Request $request)
+    // {
+    //     $wishlist = new Wishlist;
+    //     $wishlist->user_id = $request->user()->id;
+    //     $wishlist->product_id = $request->input('product_id');
+    //     $wishlist->save();
+
+    //     return response()->json($wishlist);
+    // }
 
     public function destroy(Request $request, $id)
     {
